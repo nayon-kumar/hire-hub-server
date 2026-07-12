bab9aa7395270125cb652a128dd3d09b198f1108
Sree Nayon Kumar Pal <nayon.cse55@gmail.com>
2026-07-12 15:26:32 +0600

    Subscriptions api added

--- index.js
+++ index.js
@@ -32,7 +32,16 @@ async function run() {
     const database = client.db("hire-hub");
     const jobCollection = database.collection("jobs");
     const companyCollection = database.collection("companies");
+    const usersCollection = database.collection("user");
     const applicationsCollection = database.collection("applications");
+    const planCollection = database.collection("plans");
+    const subscriptionCollection = database.collection("subscriptions");
+
+    app.get("/api/users", async (req, res) => {
+      const cursor = usersCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     app.get("/api/jobs", async (req, res) => {
       const query = {};
@@ -55,6 +64,7 @@ async function run() {
       const result = await jobCollection.findOne(query);
       res.send(result);
     });
+
     app.post("/api/jobs", async (req, res) => {
       const job = req.body;
       const newJob = {
@@ -65,9 +75,22 @@ async function run() {
       res.send(result);
     });
 
-    // Application related api
+    // application related apis
+    app.get("/api/applications", async (req, res) => {
+      const query = {};
+      if (req.query.applicantId) {
+        query.applicantId = req.query.applicantId;
+      }
+      if (req.query.jobId) {
+        query.jobId = req.query.jobId;
+      }
+      const cursor = applicationsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     app.post("/api/applications", async (req, res) => {
-      const application = req.body();
+      const application = req.body;
       const newApplication = {
         ...application,
         createdAt: new Date(),
@@ -76,7 +99,7 @@ async function run() {
       res.send(result);
     });
 
-    // Company related api
+    // company related apis
     app.get("/api/companies", async (req, res) => {
       const cursor = companyCollection.find();
       const result = await cursor.toArray();
@@ -89,13 +112,54 @@ async function run() {
         query.recruiterId = req.query.recruiterId;
       }
       const result = await companyCollection.findOne(query);
+
       res.send(result || {});
     });
 
     app.post("/api/companies", async (req, res) => {
       const company = req.body;
-      const result = await companyCollection.insertOne(company);
+      const newCompany = {
+        ...company,
+        createdAt: new Date(),
+      };
+      const result = await companyCollection.insertOne(newCompany);
       res.send(result);
+    });
+
+    // plans
+    app.get("/api/plans", async (req, res) => {
+      const query = {};
+      if (req.query.plan_id) {
+        query.id = req.query.plan_id;
+      }
+      const plan = await planCollection.findOne(query);
+      res.send(plan);
+    });
+
+    // subscription
+    app.post("/api/subscriptions", async (req, res) => {
+      const data = req.body;
+      const subsInfo = {
+        ...data,
+        createdAt: new Date(),
+      };
+
+      const result = await subscriptionCollection.insertOne(subsInfo);
+
+      // update the user plan information
+      const filter = { email: data.email };
+      // update the value of the 'quantity' field to 5
+      const updateDocument = {
+        $set: {
+          plan: data.planId,
+        },
+      };
+
+      const updateResult = await usersCollection.updateOne(
+        filter,
+        updateDocument,
+      );
+      res.send(updateResult);
     });
 
     // Send a ping to confirm a successful connection
