a3ef7af596053141d08e8039a8028738829e21d7
Sree Nayon Kumar Pal <nayon.cse55@gmail.com>
2026-07-24 11:23:51 +0600

    Company status update admin verificaion added

--- index.js
+++ index.js
@@ -232,20 +232,25 @@ async function run() {
       res.send(result);
     });
 
-    app.patch("/api/companies/:id", verifyToken, async (req, res) => {
-      const id = req.params.id;
-      const updatedCompany = req.body;
-      const filter = {
-        _id: new ObjectId(id),
-      };
-      const updatedDoc = {
-        $set: {
-          status: updatedCompany.status,
-        },
-      };
-      const result = await companyCollection.updateOne(filter, updatedDoc);
-      res.send(result);
-    });
+    app.patch(
+      "/api/companies/:id",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const id = req.params.id;
+        const updatedCompany = req.body;
+        const filter = {
+          _id: new ObjectId(id),
+        };
+        const updatedDoc = {
+          $set: {
+            status: updatedCompany.status,
+          },
+        };
+        const result = await companyCollection.updateOne(filter, updatedDoc);
+        res.send(result);
+      },
+    );
 
     // plans
     app.get("/api/plans", async (req, res) => {
